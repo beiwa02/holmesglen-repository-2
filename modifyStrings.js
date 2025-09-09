9c876f613934e6f722e7c59df12eae31df53372a
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 13:00:46 +1000

    Q3.3

---
+++ modifyStrings.js
@@ -0,0 +1,11 @@
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Bailie";
+
+// Q3.3: Slice and substring
+let myStringSlice = myString.slice(5, 9);
+let anotherStringSubstring = anotherString.substring(6, 9);
+
+console.log(myStringSlice);
+console.log(anotherStringSubstring);
