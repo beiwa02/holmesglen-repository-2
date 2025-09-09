29ebd72c913687fdf6ea6c3a3d247df9886a104e
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 13:31:27 +1000

    Q3.5

--- modifyStrings.js
+++ modifyStrings.js
@@ -13,3 +13,14 @@ console.log(anotherStringSubstring);
 // Q3.4: Upper- and lowercase
 console.log(myName.toUpperCase());
 console.log(myName.toLowerCase());
+
+// Q3.5: Concat, trim, replace, split
+let concat = hello.concat(" " + myName);
+let trim = anotherString.trim();
+let replace = myString.replace("is a", " ");
+let split = myString.split(" ");
+
+console.log("concat: " + concat);
+console.log("trim: " + trim);
+console.log("replace: " + replace);
+console.log("split: " + split);
