71b668c11193c8f89277614d39704d7bba21aafa
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 12:49:36 +1000

    Q3.1

---
+++ createStrings.js
@@ -0,0 +1,10 @@
+// Q3.1: Define strings
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Bailie";
+
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
