585b71581dc3a83cb600f14cff888612dcd86d93
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 13:40:56 +1000

    Q3.6

---
+++ readWriteFiles.js
@@ -0,0 +1,22 @@
+// Q3.6: Algorithms to write and read files
+function writeAFile(fileName, fileContent) {
+  const fs = require('fs');
+  fs.writeFile(fileName, fileContent, (err) => {
+    if (err) {
+      console.error("Error while writing file: ", err);
+      return;
+    }
+    console.log("File written successfully");
+  })
+}
+
+function readAFile(filePath) {
+  const fs = require('fs');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error("Error while reading file: ", err);
+      return;
+    }
+    console.log("File: ", data);
+  })
+}
